38498c25e250d1528ac1dbd355200f48f2a23291
ashhhhk <ashvinkumhar2002@gmail.com>
2024-05-19 23:40:29 +0530

    few changes

--- mfe1/webpack.config.js
+++ mfe1/webpack.config.js
@@ -5,37 +5,38 @@ const path = require("path");
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
   path.join(__dirname, 'tsconfig.json'),
-  [/* mapped paths to share */]);
+  [/* Add paths you want to share */]
+);
 
 module.exports = {
   output: {
     uniqueName: "mfe1",
-    publicPath: "auto"
+    publicPath: "auto",
   },
   optimization: {
-    runtimeChunk: false
-  },   
+    runtimeChunk: false,
+  },
   resolve: {
     alias: {
       ...sharedMappings.getAliases(),
-    }
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "mfe1",
       filename: "remoteEntry.js",
       exposes: {
-        './OrderModule': './/src/app/order/order.module.ts',
+        './OrderModule': './src/app/order/order.module.ts', // Fixed path
       },
       shared: {
-        "@angular/core": { singleton: true, strictVersion: true }, 
-        "@angular/common": { singleton: true, strictVersion: true }, 
-        "@angular/common/http": { singleton: true, strictVersion: true }, 
+        "@angular/core": { singleton: true, strictVersion: true },
+        "@angular/common": { singleton: true, strictVersion: true },
+        "@angular/common/http": { singleton: true, strictVersion: true },
         "@angular/router": { singleton: true, strictVersion: true },
 
-        ...sharedMappings.getDescriptors()
-      }
+        ...sharedMappings.getDescriptors(),
+      },
     }),
-    sharedMappings.getPlugin()
+    sharedMappings.getPlugin(),
   ],
 };
