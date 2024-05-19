db9e821dffe84960625a208fe085610b7c3a8976
ashhhhk <ashvinkumhar2002@gmail.com>
2024-05-19 23:53:50 +0530

    changes

--- mfe2/webpack.config.js
+++ mfe2/webpack.config.js
@@ -2,51 +2,49 @@ const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPl
 const mf = require("@angular-architects/module-federation/webpack");
 const path = require("path");
 
-
 const sharedMappings = new mf.SharedMappings();
 sharedMappings.register(
   path.join(__dirname, 'tsconfig.json'),
-  [/* mapped paths to share */]);
+  [/* Add paths you want to share */]
+);
 
 module.exports = {
   output: {
     uniqueName: "mfe2",
-    publicPath: "auto"
+    publicPath: "auto",
   },
   optimization: {
-    runtimeChunk: false
-  },   
+    runtimeChunk: false,
+  },
   resolve: {
     alias: {
       ...sharedMappings.getAliases(),
-    }
+    },
   },
   plugins: [
     new ModuleFederationPlugin({
+      // For remotes (please adjust)
+      name: "mfe2",
+      filename: "remoteEntry.js",
+      exposes: {
+        './Component': './src/app/app.component.ts', // Fixed path
+      },
       
-        // For remotes (please adjust)
-        name: "mfe2",
-        filename: "remoteEntry.js",
-        exposes: {
-            './Component': './/src/app/app.component.ts',
-        },        
-        
-        // For hosts (please adjust)
-        // remotes: {
-        //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
+      // For hosts (please adjust)
+      // remotes: {
+      //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
+      // },
 
-        // },
+      shared: {
+        "@angular/core": { singleton: true, strictVersion: true },
+        "@angular/common": { singleton: true, strictVersion: true },
+        "@angular/common/http": { singleton: true, strictVersion: true },
+        "@angular/router": { singleton: true, strictVersion: true },
 
-        shared: {
-          "@angular/core": { singleton: true, strictVersion: true }, 
-          "@angular/common": { singleton: true, strictVersion: true }, 
-          "@angular/common/http": { singleton: true, strictVersion: true }, 
-          "@angular/router": { singleton: true, strictVersion: true },
-
-          ...sharedMappings.getDescriptors()
-        }
-        
+        ...sharedMappings.getDescriptors(),
+      },
     }),
-    sharedMappings.getPlugin()
+    sharedMappings.getPlugin(),
   ],
 };
+
